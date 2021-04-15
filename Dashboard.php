58837611e1d89db1de0ed1cac999aac884402c8a
jjames2699 <43121611+jjames2699@users.noreply.github.com>
2021-04-15 14:12:37 -0700

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -1,6 +1,7 @@
 <?php
+ include "Connection.php";
 include "Includes.php";
-if(isset($_SESSION['username']))
+if(isset($_SESSION['username'])) //this is a comment
 {
 ?>
 <!DOCTYPE html>
@@ -12,12 +13,23 @@ if(isset($_SESSION['username']))
 <body>
 
 
+
 <div>
 <?php //<a href = http://localhost/Loginform.php><button id = "signIn">  Sign In  </button></a> ?>
 <?php //<button id = "signUp"> Sign Up </button> ?>
 <a href = "Logout.php"><button id = "signOut">   Sign Out </button></a>
 <a href = "Profile.php"> <button id = editProfile> Edit Profile </button> </a>
-<p id = "userNameDisplay"> <?php  $Color = "white";  echo '<div style="Color:'.$Color.'">'.$_SESSION['username'].'</div>';  ?> </p>
+<p id = "userNameDisplay" style= "color:white;">  <?php  $Color = "white"; echo 'Welcome Back '.'<i style="Color:'.$Color.'">'.$_SESSION['username'].'</i>';  ?>  <?php
+$sql = "SELECT * FROM users2 WHERE username = '".$_SESSION['username']."'";
+$sth = $conn->query($sql);
+$result=mysqli_fetch_assoc($sth);
+$profpic = $result['profilepic'];
+$imgUrl = "photos/$profpic";
+echo '<a href = "Profile.php"><img src="'.$imgUrl.'" id="output" height="25" width="25"> </a>' ;
+?> </p> 
+
+
+
 <button id = "explore"> Explore </button>
 </div>
 
