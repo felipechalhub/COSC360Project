b1a049c71ef9ff8701c091daa2ed0f3d2b29ccb7
jjames2699 <43121611+jjames2699@users.noreply.github.com>
2021-04-15 14:13:52 -0700

    Update Profile_update.php

--- Profile_update.php
+++ Profile_update.php
@@ -9,24 +9,35 @@
     $lastname=$_POST['lastname'];
     $email=$_POST['email'];
     $password = $_POST['pword'];
-    $select= "select * from users1 where username='$username'";
+    //$image = $_POST['image'];
+    $profilepic=$_FILES['image'];
+    move_uploaded_file($profilepic['tmp_name'],"photos/".$profilepic['name']);
+
+
+    $select= "select * from users2 where username='$username'";
     $sql = mysqli_query($conn,$select);
     $row = mysqli_fetch_assoc($sql);
     $res= $row['username'];
+    
     if($res === $username)
     {
+        
    
-       $update = "update users1 set firstname = '$firstname', lastname ='$lastname', email='$email', password='$password' where username='$username'";
+       $update = "update users2 set firstName = '$firstname', lastName ='$lastname', email='$email', password='$password', profilepic='$profilepic[name]' where username='$username'";
        $sql2=mysqli_query($conn,$update);
 if($sql2)
        { 
-           
+        //echo "<script type='text/javascript'>alert('Profile Editted');</script>";
            header('location:Dashboard.php');
+           //echo "<script type='text/javascript'>alert('Profile Editted');</script>";
        }
        else
        {
           
            header('location:Profile_edit_form.php');
+           //echo "<script type='text/javascript'>alert('Profile Editted');</script>";
+          // $_SESSION['message'] = 'success';
+           //echo  $_SESSION['message'];
        }
     }
     else
@@ -34,5 +45,7 @@ if($sql2)
         
         header('location:Profile_edit_form.php');
     }
+    
  }
+ 
 ?>
